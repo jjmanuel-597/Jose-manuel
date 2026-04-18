c3bb71f57118f91932c5f64971bd594c61ca119f
jjmanuel-597 <jjmanuel597@gmail.com>
2026-04-18 08:13:26 -0400

    Add files via upload

---
+++ Ejercicio 2.py
@@ -0,0 +1,18 @@
+
+
+numero = int(input("ingrese su numero entero:"))
+
+if numero == 5:
+    print("el numero es cero")
+
+elif numero > 0: 
+
+    print("el numero es positivo")
+
+else:
+
+    print("el numero es muerto")
+
+if numero < 0:
+   
+   print("el numero es negativo")
