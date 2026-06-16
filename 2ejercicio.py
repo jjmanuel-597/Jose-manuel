b3258c03b0dddcb33bc859836de54184abad9b88
jjmanuel-597 <jjmanuel597@gmail.com>
2026-06-15 22:57:25 -0400

    Update 2ejercicio.py

--- 2ejercicio.py
+++ 2ejercicio.py
@@ -1,12 +1,11 @@
 """Enunciado: Calcula la suma de todos los números del 1 hasta un límite ingresado por el usuario.
 Requerimientos: Usa un bucle for, la función range() y el operador de asignación +="""
 
-# Solicitar al usuario que ingrese un límite entero
 limite = int(input("Ingrese un número entero como límite: "))
-# Inicializar la variable para almacenar la suma
+
 suma = 0
-# Utilizar un bucle for para iterar desde 1 hasta el límite
+
 for numero in range(1, limite + 1):
-    suma += numero  # Sumar el número actual a la variable suma
-# Imprimir el resultado de la suma
+    suma += numero  
+
 print(f"La suma de los números del 1 al {limite} es: {suma}.")
