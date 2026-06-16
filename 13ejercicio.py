84e818a222865e267f0e5772b102a76e616bc23d
jjmanuel-597 <jjmanuel597@gmail.com>
2026-06-15 22:54:39 -0400

    Update 13ejercicio.py

--- 13ejercicio.py
+++ 13ejercicio.py
@@ -2,15 +2,15 @@
 Requerimientos: Usa .split(), y un bucle for para llenar un diccionario usando condicionales o el operador in."""
 
 def contar_palabras(parrafo):
-    palabras = parrafo.lower().split()  # Convertir a minúsculas y dividir el párrafo en palabras
+    palabras = parrafo.lower().split() 
     conteo = {}
     for palabra in palabras:
         if palabra in conteo:
-            conteo[palabra] += 1  # Incrementar el conteo si la palabra ya está en el diccionario
+            conteo[palabra] += 1 
         else:
-            conteo[palabra] = 1  # Agregar la palabra al diccionario con un conteo inicial de 1
+            conteo[palabra] = 1 
     return conteo
-# Ejemplo de uso
+
 parrafo = "Hola mundo. Hola a todos. El mundo es grande."
 resultado = contar_palabras(parrafo)
-print(resultado)  # {'hola': 2, 'mundo.': 1, 'a': 1, 'todos.': 1, 'el': 1, 'mundo': 1, 'es': 1, 'grande.': 1}
+print(resultado) 
