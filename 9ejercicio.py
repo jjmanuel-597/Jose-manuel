88c00cb4f51f5349eee1bae3bbfc0daa809defa8
jjmanuel-597 <jjmanuel597@gmail.com>
2026-06-15 22:58:39 -0400

    Update 9ejercicio.py

--- 9ejercicio.py
+++ 9ejercicio.py
@@ -5,4 +5,4 @@ for i in range(1, 6):
     print(f"Tabla de multiplicar del {i}:")
     for j in range(1, 11):
         print(f"{i} x {j} = {i * j}")
-    print()  # Línea en blanco para separar las tablas
+    print() 
