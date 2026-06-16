64e7b4e772b937cbaf4f435189139c96127d3c6b
jjmanuel-597 <jjmanuel597@gmail.com>
2026-06-15 22:53:34 -0400

    Refactor multiplication example with bitwise operators

--- 12ejercicio.py
+++ 12ejercicio.py
@@ -5,8 +5,8 @@ def multiplicar_por_dos_y_cuatro(numero):
     resultado_dos = numero << 1  # Multiplica por 2 desplazando a la izquierda 1 bit
     resultado_cuatro = numero << 2  # Multiplica por 4 desplazando a la izquierda 2 bits
     return resultado_dos, resultado_cuatro
-# Ejemplo de uso
+
 numero = 5
 resultado_dos, resultado_cuatro = multiplicar_por_dos_y_cuatro(numero)
-print(f"{numero} multiplicado por 2 es: {resultado_dos}")  # 5 multiplicado por 2 es: 10
-print(f"{numero} multiplicado por 4 es: {resultado_cuatro}")  # 5 multiplicado por 4 es: 20 
+print(f"{numero} multiplicado por 2 es: {resultado_dos}")  
+print(f"{numero} multiplicado por 4 es: {resultado_cuatro}") 
