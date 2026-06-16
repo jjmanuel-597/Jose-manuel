194ffb741fcd9d6d7d6fc6cb9513a1eed497fd3c
jjmanuel-597 <jjmanuel597@gmail.com>
2026-06-15 22:55:38 -0400

    Update 15ejercicio.py

--- 15ejercicio.py
+++ 15ejercicio.py
@@ -1,7 +1,6 @@
 """Enunciado: Suma dos matrices 3x3 elemento por elemento y crea una matriz resultado.
 Requerimientos: Emplea listas de listas, bucles for anidados e indexación doble [i][j]."""
 
-# Definir las matrices A y B
 A = [
     [1, 2, 3],
     [4, 5, 6],
@@ -12,17 +11,16 @@ B = [
     [6, 5, 4],
     [3, 2, 1]
 ]
-# Crear una matriz resultado inicializada con ceros
+
 resultado = [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0]
 ]
-# Sumar las matrices A y B elemento por elemento
 for i in range(3):
     for j in range(3):
         resultado[i][j] = A[i][j] + B[i][j]
-# Imprimir la matriz resultado
+
 print("Matriz A:")
 for fila in A:
     print(fila)
