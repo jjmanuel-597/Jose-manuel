db8a5fc4e313fe3bdd876c7327c00a3133d5cad6
jjmanuel-597 <jjmanuel597@gmail.com>
2026-06-15 22:56:21 -0400

    Update 1ejercicio.py

--- 1ejercicio.py
+++ 1ejercicio.py
@@ -1,9 +1,8 @@
 """Enunciado: Crea un programa que pida un número entero y determine si es par o impar.
 Requerimientos: Usa input(), el operador de módulo (%) y una estructura condicional if-else"""
 
-# Solicitar al usuario que ingrese un número entero
 numero = int(input("Ingrese un número entero: "))
-# Verificar si el número es par o impar utilizando el operador de módulo
+
 if numero % 2 == 0:
     print(f"El número {numero} es par.")
 else:    print(f"El número {numero} es impar.")
